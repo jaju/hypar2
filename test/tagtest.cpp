3276cdfd5770e9f12b8391598eae46dcc12c31b1
Ravindra R. Jaju <jaju@msync.org>
2012-02-09 17:21:01 +0530

    Test for tag attributes not-in-quotes.

--- test/tagtest.cpp
+++ test/tagtest.cpp
@@ -28,6 +28,17 @@ TEST(Tag, ShouldParseAttributesOfATag) {
     ASSERT_STREQ(t.findProperty("foo"), "bar > and > baaz");
 }
 
+TEST(Tag, ShouldParseAttributesWithoutQuotes) {
+    const char *tag = "body bgcolor=white fgcolor='black and white' foo=bar";
+    textutils::CBuffer c(tag, strlen(tag));
+    Tag t(c.get(), true);
+
+    ASSERT_STREQ(t.m_pName, "body");
+    ASSERT_STREQ(t.findProperty("bgcolor"), "white");
+    ASSERT_STREQ(t.findProperty("fgcolor"), "black and white");
+    ASSERT_STREQ(t.findProperty("foo"), "bar");
+}
+
 TEST(Tag, ShouldIdentifyEndTags) {
     const char *tag = "/html bgcolor=\"white\"";
     textutils::CBuffer c(tag, strlen(tag));
