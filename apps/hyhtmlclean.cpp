56d17fa1d9a9e1bee25f14b1765b94982c90406a
Ravindra R. Jaju <jaju@msync.org>
2012-02-10 00:07:26 +0530

    HTML cleaner app - sample.

---
+++ apps/hyhtmlclean.cpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2004 Ravindra Jaju (jaju [AT] msync org)
+ */
+
+/*
+ * This file is part of the HyParSuite and usage is governed by the License
+ * included with the source-code
+ */
+
+#include "localdefs.h"
+#include "mmap.hpp"
+#include "mbtowide.hpp"
+#include "entitystream.hpp"
+#include "html.hpp"
+#include "hash.hpp"
+#include "hashset.hpp"
+
+#if !DEBUG
+#define DEBUG 0
+#endif
+#include "debug.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <string>
+
+BEGIN_C_DECLS;
+#include <assert.h>
+#include <stdio.h>
+#include <locale.h>
+END_C_DECLS;
+
+using namespace std;
+#if __GNUC__ > 2
+using namespace __gnu_cxx;
+#endif
+using namespace hy;
+
+using hy::Mmap;
+
+typedef HashSet<const _char *, strcasehash, eqcase> StringSet;
+
+
+void usage (int argc, char *argv[])
+{
+    cerr << "Usage: " << argv[0] << " <file>" << endl;
+}
+
+
+int main (int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        usage(argc, argv);
+        return -1;
+    }
+    setlocale (LC_ALL, "en_IN.UTF-8");
+    Mmap m (argv[1]);
+    _char *pBuffer = 0;
+#if USE_WIDECHAR
+    hy::MbToWide toWideCharConverter ("UTF-8");
+    pBuffer = reinterpret_cast<_char *> (toWideCharConverter.convert (
+                m.getBuffer (), m.getBufferLength ()
+                ));
+#else
+    int len = m.getBufferLength () + 1;
+    pBuffer = new char[len];
+    bzero (pBuffer, len);
+    memcpy (pBuffer, m.getBuffer (), len - 1);
+#endif
+    HTMLDoc h(HTMLDoc::kMEMBUF, pBuffer);
+    DOMNode *node = DOMNode::create();
+    node->setType(DOMNode::ELEMENT);
+    node->setName(L("root"));
+    node->setLevel(-1);
+    DOMNode *pNode = h.getRootNode();
+
+    _string text = L("");
+    toString(pNode->child(), text);
+
+    char *pLocalLocaleBuffer = new char[text.size () * 4 + 4];
+    strncpy (pLocalLocaleBuffer, text.c_str (), text.size () * 4 + 4);
+    cout << pLocalLocaleBuffer << endl;
+
+    delete pLocalLocaleBuffer;
+    delete pBuffer;
+    delete node;
+
+    return 0;
+}
