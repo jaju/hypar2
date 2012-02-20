053151b55c0063069be54e03ddfdead557d3f05e
Ravindra R. Jaju <jaju@msync.org>
2012-02-20 17:26:53 +0530

    Completed the test - it was just a dummy until now.

--- test/tokenizertest.cpp
+++ test/tokenizertest.cpp
@@ -6,17 +6,23 @@
 
 using std::string;
 
-TEST(XMLTokenizer, DoesNothing) {
+TEST(XMLTokenizer, IsVeryForgiving) {
     string token;
     string input = "<hello attrib=\"helloworld<<<>><<>'''\">world</hello attrib='wdjhskjdfjkf<>'><body>How are you?<i>YOU!</i></body>";
+    typedef boost::tokenizer<XMLTokenizerFunc> T;
 
-    boost::tokenizer<XMLTokenizerFunc> tok(input);
+    T tok(input);
+    std::vector<string> tokens;
+    for (T::iterator x = tok.begin(); x != tok.end(); tokens.push_back(*x), x++);
 
-    boost::tokenizer<XMLTokenizerFunc>::iterator x;
-
-    x = tok.begin();
-    while (x != tok.end()) {
-        std::cout << *x << std::endl;
-        x++;
-    }
+    ASSERT_EQ(tokens[0], "<hello attrib=\"helloworld<<<>><<>'''\">");
+    ASSERT_EQ(tokens[1], "world");
+    ASSERT_EQ(tokens[2], "</hello attrib='wdjhskjdfjkf<>'>");
+    ASSERT_EQ(tokens[3], "<body>");
+    ASSERT_EQ(tokens[4], "How are you?");
+    ASSERT_EQ(tokens[5], "<i>");
+    ASSERT_EQ(tokens[6], "YOU!");
+    ASSERT_EQ(tokens[7], "</i>");
+    ASSERT_EQ(tokens[8], "</body>");
+    
 }
