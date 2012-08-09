88e52b453f9fb60f4b336c17f1cd87d9ad875ad9
Ravindra R. Jaju <jaju@msync.org>
2012-08-09 15:29:54 +0530

    Test for preceding and trailing spaces in text elements - and use EXPECT instead of ASSERT.

--- test/tokenizertest.cpp
+++ test/tokenizertest.cpp
@@ -8,21 +8,21 @@ using std::string;
 
 TEST(XMLTokenizer, IsVeryForgiving) {
     string token;
-    string input = "<hello attrib=\"helloworld<<<>><<>'''\">world</hello attrib='wdjhskjdfjkf<>'><body>How are you?<i>YOU!</i></body>";
+    string input = "<hello attrib=\"helloworld<<<>><<>'''\"> world</hello attrib='wdjhskjdfjkf<>'><body>How are you? <i> YOU! </i></body>";
     typedef boost::tokenizer<XMLTokenizerFunc> T;
 
     T tok(input);
     std::vector<string> tokens;
     for (T::iterator x = tok.begin(); x != tok.end(); tokens.push_back(*x), x++);
 
-    ASSERT_EQ(tokens[0], "<hello attrib=\"helloworld<<<>><<>'''\">");
-    ASSERT_EQ(tokens[1], "world");
-    ASSERT_EQ(tokens[2], "</hello attrib='wdjhskjdfjkf<>'>");
-    ASSERT_EQ(tokens[3], "<body>");
-    ASSERT_EQ(tokens[4], "How are you?");
-    ASSERT_EQ(tokens[5], "<i>");
-    ASSERT_EQ(tokens[6], "YOU!");
-    ASSERT_EQ(tokens[7], "</i>");
-    ASSERT_EQ(tokens[8], "</body>");
+    EXPECT_EQ(tokens[0], "<hello attrib=\"helloworld<<<>><<>'''\">");
+    EXPECT_EQ(tokens[1], " world");
+    EXPECT_EQ(tokens[2], "</hello attrib='wdjhskjdfjkf<>'>");
+    EXPECT_EQ(tokens[3], "<body>");
+    EXPECT_EQ(tokens[4], "How are you? ");
+    EXPECT_EQ(tokens[5], "<i>");
+    EXPECT_EQ(tokens[6], " YOU! ");
+    EXPECT_EQ(tokens[7], "</i>");
+    EXPECT_EQ(tokens[8], "</body>");
     
 }
